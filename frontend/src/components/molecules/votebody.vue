4220afbd9d5a0390ac0e074cdeb7ee055a581e13
ItoNatsuki <mametann72@gmail.com>
2022-10-03 11:07:11 +0900

    wip

---
+++ frontend/src/components/molecules/votebody.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="voteBody">
+    <h1>{{questions.title}}</h1>
+    <voteButtons :choices="questions.choices" @voteObj = "voteObj"/>
+    <br>
+    <button @click="refreshClick">更新</button>
+</div>
+
+</template>
+
+<script>
+import voteButtons from '@/components/molecules/voteButtons'
+export default {
+    components:{
+        voteButtons
+    },
+    props:['questions'],
+    methods:{
+        voteObj(payload){
+            this.$emit("vote",payload)
+        },
+        refreshClick(){
+            this.$emit("refreshClick");
+        }
+    }
+}
+</script>
+<style >
+</style>
